a485c39930d719a2e00aef6382e984d7fd382c73
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-10 11:06:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -113,15 +113,15 @@ banner = f"""
 {Fore.WHITE}  /  /__\  \  |  |    |  |  \______  \  |  |_____|  |   /   /___\  \ 
 {Fore.WHITE} /  _____\  \ |  \____/  /   ______| |  |   _____   |  /   _______  \
 {Fore.WHITE}/__/      \__\ \_____   /   |________/  |__|     |__| /___/       \__\
-{Fore.WHITE}                     \__\{Fore.RESET}"""
-banner = f"""
+{Fore.WHITE}                     \__\
+{Fore.RED}
 {Fore.RED}╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
 {Fore.RED}║{Fore.WHITE}             THIS SCRIPT IS DEDICATED TO THEM _   {Fore.RED}║
 {Fore.RED}║{Fore.GREEN}               WHO HAS A HIGH FIGHTING SPIRIT     {Fore.RED}║
 {Fore.RED}║{Fore.YELLOW}                 FOR OUR BELOVED PALESTINE       {Fore.RED}║
 {Fore.RED}║{Fore.CYAN}                           ———oO0Oo——              {Fore.RED}║
-{Fore.RED}╩━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━══╝{Fore.RESET}")
-TOOL #{Fore.RESET}"""
+{Fore.RED}╩━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━══╝
+{Fore.WHITE}TOOL INI MEMBUTUHKAN WAKTU 35 DETIK UTK MEMULAI SERANGAN {Fore.RESET}"""
 print(banner)
 host = ""
 ip = ""

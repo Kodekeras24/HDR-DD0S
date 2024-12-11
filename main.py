fbb66a506a181f9102960fce227a3f5b833535c0
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-11 07:31:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -26,14 +26,7 @@ class SockFlood:
 					   ░   ░ ░                 ░  ░    ░ ░      ░ ░     ░    
 						   ░ ░                                        ░      
 		"""
-		print(Fore.RED+banner)
-		print(Fore.YELLOW+"""
-		[+] An Advance DDOS Tool Using Sockets Written in Python [+]"""+Fore.GREEN+"""
-		[+] Developer : Kanao#7218 [ """+Fore.WHITE+"""SecretsX ]""")
-		print(Fore.WHITE+"""
-		[+] Type `help` If You Are A Beginner [+]
-			""")
-
+		
 	def start_attack(self,host,port=None):
 		self.sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 		try:

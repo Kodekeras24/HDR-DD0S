f2b1bb1aa232302d94df481f19116c2ed1c70bd1
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-11 18:01:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,15 @@ def main() -> None:
     show_logo()
     try:
             if (method := check_method_input()) in ["arp-spoof", "disconnect"]:()
-        target = ()
+        
+def Factorial(n): # return factorial
+    result = 1
+    for i in range (1,n):
+        result = result * i
+    print "factorial is ",result
+    return result
+
+print Factorial(10)
             check_http_target_input()
             if method not in ["arp-spoof", "disconnect"]
             else check_local_target_input()

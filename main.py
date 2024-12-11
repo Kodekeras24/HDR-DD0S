3a063c9d77f7cabd575a2f61553a4757e0934550
Kodekeras24 <zhanahmad57@gmail.com>
2024-12-12 01:07:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def countdown(t):
     until = datetime.datetime.now() + datetime.timedelta(seconds=int(t))
     while True:
         remaining_time = (until - datetime.datetime.now()).total_seconds()
-        if remaining_time > 2:
+        if remaining_time > 0:
             stdout.flush()
             stdout.write(f"\r{Fore.BLUE}|[*] {Fore.YELLOW}HUDAIRUL AQSHA {Fore.WHITE} Attack'ts  {Fore.RED}{remaining_time:.2f}  {Fore.BLUE}Sec left {' ' * 26}|")
             stdout.write(f"\r{Fore.YELLOW}|[*] {Fore.CYAN}HUDAIRUL AQSHA {Fore.RED} Attack'ts  {Fore.GREEN}{remaining_time:.2f}  {Fore.MAGENTA}Sec left {' ' * 26}|")
